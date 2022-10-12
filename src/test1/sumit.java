d5d6970afcc042a382a7fe6141ef7e256aa31132
hp <hp@laptop>
2022-10-12 22:42:00 +0530

    test commit

---
+++ src/test1/sumit.java
@@ -0,0 +1,14 @@
+package test1;
+
+public class sumit {
+	
+	public static void main(String[] args) {
+		int a= 200;
+	    int b =300;
+	    int c= a+b;
+	    
+	    System.out.println(c);
+	}
+
+}
+
